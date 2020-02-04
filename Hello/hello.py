930878cdc88eb38992bd83131180896d99430064
anacrj <a.cojocaru.2019@alumnos.urjc.es>
2020-02-04 16:01:54 +0100

    Session 1. Exercise 1

--- Hello/hello.py
+++ Hello/hello.py
@@ -1,6 +1,7 @@
-print("This is my first python program in pycharm.")
+print("This is my first python program in pycharm.") #estos puntos se llaman brak point e indica una pausa en esta condicion
 print("Hello world!!! Changed!")
 
 # -- Exercise S1-1
 print ("Another print added!")
 print ("just testing...")
+
