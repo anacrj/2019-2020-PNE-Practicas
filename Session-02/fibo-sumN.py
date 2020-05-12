628c685dbbdc31ad566b4b69f2eb8efc21fba9c9
ancaa <a.cojocaru.2019>
2020-05-13 01:31:38 +0200

    Session-02. Exercise 3

--- Session-02/fibo-sumN.py
+++ Session-02/fibo-sumN.py
@@ -4,21 +4,14 @@
 def fibosum(n):
     x1 = 0
     x2 = 1
-    list_fibon = []
-    if n >= 1:
-        list_fibon.append(x1)
-    if n >= 2:
-        list_fibon.append(x2)
-    if n >= 3:
-        for n in range(3, n + 1):
-            xn = x1 + x2
-            list_fibon.append(xn)
-            x1 = x2
-            x2 = xn
-    return list_fibon
+    result = 0
+    for i in range(n):
+        x3 = x1 + x2
+        x1 = x2
+        x2 = x3
+        result = result + x1
+    return result
 
 
-fibonacci = fibosum(30)
-
-print("Sum of the first 5 terms of the Fibonacci series: ", fibonacci[5])
-print("Sum of the first 10 terms of the Fibonacci series: ", fibonacci[10])
+print("Sum of the first 5 terms of the Fibonacci series: ", fibosum(5))
+print("Sum of the first 10 terms of the Fibonacci series: ", fibosum(10))
