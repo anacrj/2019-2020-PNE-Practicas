974f81257ce82c910293b029be0082ef63fc95bb
ancaa <a.cojocaru.2019>
2020-05-21 19:08:37 +0200

    Practice 0. Exercise 1.

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,2 +1,17 @@
+from pathlib import Path
+
+# Exercise 1: seq_ping
 def seq_ping():
     print("OK!")
+
+# Exercise 2: seq_read_fasta()
+def seq_read_fasta(filename):
+    file_contents = Path(filename).read_text()
+    f = file_contents.split("\n")
+    chain = "\n".join(f[1:])
+    return chain
+
+
+
+
+
