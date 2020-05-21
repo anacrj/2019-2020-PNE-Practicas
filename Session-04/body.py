e2328d99f09525a7f9d4e9ad4ddad9017f2f5cea
ancaa <a.cojocaru.2019>
2020-05-21 18:11:28 +0200

    Session-04. All Exercise 4.

--- Session-04/body.py
+++ Session-04/body.py
@@ -5,8 +5,13 @@ FILENAME = "U5.txt"
 
 # -- Open and read the file
 file_contents = Path(FILENAME).read_text()
+
+# -- Transform into lines
 f = file_contents.split("\n")
-chain = ''.join(f[1::])
+
+# -- Transform into text (The join method takes all items in an iterable and joins them into one string.)
+chain = "\n".join(f[1:])
 
 # -- Print the contents on the console
-print("The body of the U5.txt file is: ", chain)
+print("The body of the U5.txt file is: ")
+print(chain)
