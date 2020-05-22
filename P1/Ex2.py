cbbd6cf6073a04b78ff0e22826fc4ae2bda6ce2b
ancaa <a.cojocaru.2019>
2020-05-22 22:42:48 +0200

    Practice 1. Exercise 2

---
+++ P1/Ex2.py
@@ -0,0 +1,6 @@
+from Seq1 import Seq
+print("---Exercise 2---")
+sequence1 = Seq()                  # Creating a Null sequence
+sequence2 = Seq("TATAC")            # Creating a valid sequence
+print(f"Sequence 1: {sequence1}")
+print(f"Sequence 2: {sequence2}")
