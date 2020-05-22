cdfe8509683e9daf0466883f0d7d3700c3d3a047
ancaa <a.cojocaru.2019>
2020-05-23 00:36:26 +0200

    Practice 1. Exercise 7

--- P1/Ex7.py
+++ P1/Ex7.py
@@ -6,10 +6,10 @@ sequence3 = Seq("Invalid sequence")    # Create an invalid sequence
 
 print(f"Sequence 1: (Length: {sequence1.len()}) {sequence1}")
 print(f"  Bases: {sequence1.count()}")
-print(f"Rev: {sequence1.seq_reverse()}")
+print(f"  Rev: {sequence1.seq_reverse()}")
 print(f"Sequence 2: (Length: {sequence2.len()}) {sequence2}")
 print(f"  Bases: {sequence2.count()}")
-print(f"Rev: {sequence2.seq_reverse()}")
+print(f"  Rev: {sequence2.seq_reverse()}")
 print(f"Sequence 3: (Length: {sequence3.len()}) {sequence3}")
 print(f"  Bases: {sequence3.count()}")
-print(f"Rev: {sequence3.seq_reverse()}")
+print(f"  Rev: {sequence3.seq_reverse()}")
