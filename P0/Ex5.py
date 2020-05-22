8ac0f14b34a944a3f7a0b930e0ed025fb3373b57
ancaa <a.cojocaru.2019>
2020-05-22 15:42:37 +0200

    Practice 0. Exercise 5

---
+++ P0/Ex5.py
@@ -0,0 +1,15 @@
+from Seq0 import *
+FOLDER = "../Session-04/"
+FILENAME1 = "U5.txt"
+FILENAME2 = "ADA.txt"
+FILENAME3 = "FRAT1.txt"
+FILENAME4 = "FXN.txt"
+
+list_genes = [FILENAME1, FILENAME2, FILENAME3, FILENAME4]
+bases = ["A", "C", "T", "G"]
+
+print("---Exercise 5---")
+
+for element in list_genes:
+    seq = seq_read_fasta(FOLDER + element)
+    print("Gene:", element, ":", seq_count(seq))
